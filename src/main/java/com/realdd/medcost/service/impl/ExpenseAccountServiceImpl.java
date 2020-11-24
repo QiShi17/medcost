0eb02720a4dd064ace4ebfff25869d819326b25c
Weiser <xwze@bupt.edu.cn>
2020-11-24 16:53:11 +0800

    Update ExpenseAccountServiceImpl.java

--- src/main/java/com/realdd/medcost/service/impl/ExpenseAccountServiceImpl.java
+++ src/main/java/com/realdd/medcost/service/impl/ExpenseAccountServiceImpl.java
@@ -1,14 +1,19 @@
 package com.realdd.medcost.service.impl;
 
 import com.realdd.medcost.common.utils.SerialNumberUtil;
+import com.realdd.medcost.dto.AccountDetail;
 import com.realdd.medcost.entity.ExpenseAccount;
+import com.realdd.medcost.entity.Role;
+import com.realdd.medcost.mapper.AccountDetailMapper;
 import com.realdd.medcost.mapper.ExpenseAccountMapper;
+import com.realdd.medcost.mapper.RoleMapper;
 import com.realdd.medcost.service.ExpenseAccountService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
+import java.util.List;
 
 /**
  * <p>
@@ -23,10 +28,19 @@ public class ExpenseAccountServiceImpl extends ServiceImpl<ExpenseAccountMapper,
     @Autowired
     SerialNumberUtil serialNumberUtil;
 
+    @Autowired
+    AccountDetailMapper accountDetailMapper;
+
     @Override
     public boolean create(ExpenseAccount expenseAccount) {
 
         expenseAccount.setSerialNum(serialNumberUtil.getSerialNumber());
         return save(expenseAccount);
     }
+
+    @Override
+    public List<AccountDetail> getAccountDetailByExpenseAccountId(Long expenseAccountId) {
+        return this.accountDetailMapper.getAccountDetailByExpenseAccountId(expenseAccountId);
+    }
+
 }
