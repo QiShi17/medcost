bbdd2a01c06e03c5ab75189df43a2e5d09df0be9
duanduan <466395399@qq.com>
2020-12-08 23:50:57 +0800

    <修复>expenseAccount主键自增

--- src/main/java/com/realdd/medcost/entity/ExpenseAccount.java
+++ src/main/java/com/realdd/medcost/entity/ExpenseAccount.java
@@ -4,7 +4,9 @@ import java.io.Serializable;
 import java.util.Date;
 
 import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
@@ -27,6 +29,7 @@ public class ExpenseAccount implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    @TableId(value = "id", type = IdType.AUTO)
     private Long id;
 
     @ApiModelProperty(value = "流水号（时间戳17+随机码3）")
